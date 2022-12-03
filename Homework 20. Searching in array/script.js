744ee4bae916e23e7d9bad3710aab670a01ebcb6
Denys <tymchenko.denys@gmail.com>
2022-12-03 17:06:40 +0200

    code optimized

--- Homework 20. Searching in array/script.js
+++ Homework 20. Searching in array/script.js
@@ -2,18 +2,16 @@ const arr = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63,
 
 //Знайти суму та кількість позитивних елементів.
 let sum = 0;
-let positiveNums = 0;
 
-arr
-    .filter(function (number) {
-        return number >= 0;
-    })
-    .forEach(function (number, index, array) {
-        sum += number;
-        positiveNums++;
-    });
+const positiveNums = arr.filter(function (number) {
+    return number >= 0;
+})
 
-console.log(`Sum of positive numbers: ${sum}.\nAmmount of positive numbers: ${positiveNums}`);
+positiveNums.forEach(function (number) {
+    sum += number;
+})
+
+console.log(`Sum of positive numbers: ${sum}.\nAmmount of positive numbers: ${positiveNums.length}`);
 
 //Знайти мінімальний елемент масиву та його порядковий номер.
 //Знайти максимальний елемент масиву та його порядковий номер.
@@ -29,44 +27,40 @@ console.log(`The minimum array element is ${minNumber}, with index ${arr.indexOf
 console.log(`The maximum array element is ${maxNumber}, with index ${arr.indexOf(maxNumber)}`);
 
 //Визначити кількість негативних елементів.
-let negativeNums = 0;
-
-arr
+const negativeNums = arr
     .filter(function (number) {
         return number < 0;
     })
-    .forEach(function (number, index, array) {
-        negativeNums++
-    });
 
-console.log(`Ammount of negative numbers: ${negativeNums}`);
+console.log(`Ammount of negative numbers: ${negativeNums.length}`);
 
 //Знайти кількість парних позитивних елементів.
-const evenPosNums = arr.filter(function (number) {
-    return number >= 0 && number % 2 === 0;
+const positiveEvenNums = positiveNums.filter(function (number) {
+    return number % 2 === 0;
 });
 
-console.log(`Ammount of positive even numbers: ${evenPosNums.length}`)
+console.log(`Ammount of positive even numbers: ${positiveEvenNums.length}`)
 
 //Знайти кількість непарних позитивних елементів.
-const oddPosNums = arr.filter(function (number) {
-    return number >= 0 && number % 2 !== 0;
+const positiveOddNums = positiveNums.filter(function (number) {
+    return number % 2 !== 0;
 })
 
-console.log(`Ammount of positive odd numbers: ${oddPosNums.length}`);
+console.log(`Ammount of positive odd numbers: ${positiveOddNums.length}`);
 
 //Знайти суму парних позитивних елементів.
 sum = 0;
 
-evenPosNums.forEach(function (number) {
+positiveEvenNums.forEach(function (number) {
     sum += number;
 });
 
 console.log(`Sum of positive even numbers: ${sum}`);
+
 //Знайти суму непарних позитивних елементів.
 sum = 0;
 
-oddPosNums.forEach(function (number) {
+positiveOddNums.forEach(function (number) {
     sum += number;
 });
 
@@ -75,13 +69,9 @@ console.log(`Sum of positive even numbers: ${sum}`);
 //Знайти добуток позитивних елементів.
 let product = 1;
 
-arr
-    .filter(function (number) {
-        return number >= 0;
-    })
-    .forEach(function (number) {
-        product *= number;
-    });
+positiveNums.forEach(function (number) {
+    product *= number;
+});
 
 console.log(`Protuct of positive numbers: ${product}`);
 
