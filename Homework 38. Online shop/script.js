ec66b1665781edc0234adc4e08782aec63bb4d65
Denys <tymchenko.denys@gmail.com>
2023-01-24 14:44:38 +0200

    homework 38 added

---
+++ Homework 38. Online shop/script.js
@@ -0,0 +1,236 @@
+const shop = {
+    categories: {
+        'Headphones': {
+            products: [
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '100$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '60$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '35$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '40$'
+                },
+            ]
+        },
+        'Laptops': {
+            products: [
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '1500$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '600$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '350$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '400$'
+                },
+            ]
+        },
+        'Phones': {
+            products: [
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '300$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '1500$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '500$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '400$'
+                },
+            ]
+        },
+        'TVs': {
+            products: [
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '1000$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '2000$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '10000$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '800$'
+                },
+            ]
+        },
+    }
+}
+
+const body = document.querySelector('body');
+const nav = document.createElement('nav');
+const productsDiv = document.createElement('div'); //block with all products from one category
+const productPage = document.createElement('div'); //block with product's detailed info
+
+productsDiv.classList.add('category-products');
+productPage.classList.add('product-page');
+
+function addNavigation(categories) {
+    const ul = document.createElement('ul');
+
+    for (const category in categories) {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+
+        a.href = '#';
+        a.innerText = category;
+        a.addEventListener('click', () => {
+            showCategory(categories[category].products);
+        });
+
+        li.append(a);
+        ul.append(li);
+    }
+
+    nav.append(ul);
+    body.append(nav);
+};
+
+function showCategory(products) {
+    //We need to clear all inner HTMl, so we could render new items from another category, if it needed.
+    //!If we'll not do this: new product cards will stood on top of previous ones.
+    productsDiv.innerHTML = '';
+    //We also need to remove product page, so when user click on category - there's only nav and products
+    productPage.remove();
+
+    products.forEach(product => {
+        const productCard = document.createElement('div');
+        const img = document.createElement('img');
+        const productName = document.createElement('h2');
+        const productPrice = document.createElement('h3');
+
+        productCard.classList.add('product-card');
+        productCard.addEventListener('click', () => {
+            showProductPage(product);
+        });
+
+        img.setAttribute('src', product.photo);
+        img.setAttribute('width', '150px');
+
+        productName.innerText = product.name;
+        productPrice.innerText = product.price;
+
+        productCard.append(img);
+        productCard.append(productName);
+        productCard.append(productPrice);
+
+        productsDiv.append(productCard);
+    });
+
+    body.append(productsDiv);
+}
+
+function showProductPage(product) {
+    //We need to clear all inner HTMl, so we could render information for another product, if it needed. 
+    //!If we'll not do this new info will stood on top of previous one.
+    productPage.innerHTML = '';
+
+    const img = document.createElement('img');
+    const productName = document.createElement('h1');
+    const productDescription = document.createElement('h2')
+    const productPrice = document.createElement('span');
+    const button = document.createElement('button');
+
+    button.addEventListener('click', () => {
+        buy();
+    })
+
+    img.setAttribute('src', product.photo);
+    img.setAttribute('width', '300px');
+
+    productName.innerText = product.name;
+    productDescription.innerHTML = product.description;
+    productPrice.innerHTML = product.price;
+    button.innerText = 'BUY';
+
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('wrapper');
+
+    wrapper.append(productPrice);
+    wrapper.append(button);
+
+    productPage.append(img);
+    productPage.append(productName);
+    productPage.append(productDescription);
+    productPage.append(wrapper);
+
+    body.append(productPage);
+}
+
+function buy() {
+    alert('Product has been bought');
+
+    productsDiv.remove();
+    productPage.remove();
+}
+addNavigation(shop.categories);
