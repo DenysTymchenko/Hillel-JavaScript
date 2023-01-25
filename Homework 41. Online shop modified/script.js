fdb8d1c37a71f0867a19a3395c6cb5d4e0d6ef13
Denys <tymchenko.denys@gmail.com>
2023-01-25 17:02:03 +0200

    homework 41 added

---
+++ Homework 41. Online shop modified/script.js
@@ -0,0 +1,327 @@
+const shop = {
+    categories: {
+        'Headphones': {
+            products: [
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '100$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '60$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '35$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '40$'
+                },
+            ]
+        },
+        'Laptops': {
+            products: [
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '1500$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '600$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '350$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '400$'
+                },
+            ]
+        },
+        'Phones': {
+            products: [
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '300$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '1500$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '500$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '400$'
+                },
+            ]
+        },
+        'TVs': {
+            products: [
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '1000$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '2000$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '10000$'
+                },
+
+                {
+                    name: 'Product',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur debitis. Alias cupiditate architecto distinctio animi quisquam ad vitae sed velit quis ipsum, sunt voluptatibus eaque consequatur eveniet quibusdam nisi quam provident, voluptatem iure nostrum magni, maiores dolor! Fuga accusantium fugiat hic, illum quis ipsum nemo possimus labore. Ullam, expedita.',
+                    photo: 'images/product.jpg',
+                    price: '800$'
+                },
+            ]
+        },
+    }
+}
+
+const body = document.querySelector('body');
+const nav = document.querySelector('nav');
+const productsDiv = document.querySelector('.category-products'); //block with all products from one category
+const productPage = document.querySelector('.product-page'); //block with product's detailed info
+const form = document.querySelector('.product-page form'); //form for placing an order;
+form.remove();
+
+function addNavigation(categories) {
+    const ul = document.createElement('ul');
+
+    for (const category in categories) {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+
+        a.href = '#';
+        a.innerText = category;
+        a.addEventListener('click', () => {
+            showCategory(categories[category].products);
+        });
+
+        li.append(a);
+        ul.append(li);
+    }
+
+    nav.append(ul);
+};
+
+function showCategory(products) {
+    //We need to clear all inner HTMl, so we could render new items from another category, if it needed.
+    //!If we'll not do this: new product cards will stood on top of previous ones.
+    productsDiv.innerHTML = '';
+
+    products.forEach(product => {
+        const productCard = document.createElement('div');
+        const img = document.createElement('img');
+        const productName = document.createElement('h2');
+        const productPrice = document.createElement('h3');
+
+        productCard.classList.add('product-card');
+        productCard.addEventListener('click', () => {
+            showProductPage(product);
+        });
+
+        img.setAttribute('src', product.photo);
+        img.setAttribute('width', '150px');
+
+        productName.innerText = product.name;
+        productPrice.innerText = product.price;
+
+        productCard.append(img);
+        productCard.append(productName);
+        productCard.append(productPrice);
+
+        productsDiv.append(productCard);
+    });
+}
+
+function showProductPage(product) {
+    //We need to clear all inner HTMl, so we could render information for another product, if it needed. 
+    //!If we'll not do this new info will stood on top of previous one.
+    productPage.innerHTML = '';
+
+    const img = document.createElement('img');
+    const productName = document.createElement('h1');
+    const productDescription = document.createElement('h2')
+    const productPrice = document.createElement('span');
+    const button = document.createElement('button');
+
+    button.addEventListener('click', () => {
+        createOrder();
+    })
+
+    img.setAttribute('src', product.photo);
+    img.setAttribute('width', '300px');
+
+    productName.innerText = product.name;
+    productDescription.innerHTML = product.description;
+    productPrice.innerHTML = product.price;
+    button.innerText = 'BUY';
+
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('wrapper');
+
+    wrapper.append(productPrice);
+    wrapper.append(button);
+
+    productPage.append(img);
+    productPage.append(productName);
+    productPage.append(productDescription);
+    productPage.append(wrapper);
+}
+
+function createOrder() {
+    //Because of clearing productPage innerHTML before, we cleared form, that was hidden inside of it (productPage). 
+    //That's why thy we querySelected it (form) in the start of script.js: so we could add it now.
+    productPage.append(form);
+    //We also need to make it visible
+    form.classList.remove('d-none');
+
+    const cardPaymentRadioBtn = document.getElementById('card'); //one of the payment radio buttons. We need it to add event listener.
+    const postpaidPaymentRadioBtn = document.getElementById('postpaid');//one of the payment radio buttons. We need it to add event listener.
+    const cardInfo = document.querySelector('.card-info');//div in which user need to enter card information. It shows only when card payment choosed.
+    const placeOrderBtn = document.querySelector('form button');//submit button
+
+    //if user choosed payment by card, we need to show him fields, that he must fill with his card info
+    cardPaymentRadioBtn.addEventListener('click', () => {
+        cardInfo.classList.remove('d-none');
+
+        //we also need to make all those fields required
+        document.querySelectorAll('.card-info input').forEach(input => {
+            input.required = true;
+        });
+    });
+
+    //if user choosed postpaid method, we need to hide fields, that he must fill with his card info
+    postpaidPaymentRadioBtn.addEventListener('click', () => {
+        cardInfo.classList.add('d-none');
+
+        //we also need to remove required attribute from all those fields
+        document.querySelectorAll('.card-info input').forEach(input => {
+            input.required = false;
+        });
+    });
+
+    placeOrderBtn.addEventListener('click', e => {
+        e.preventDefault;
+
+        //when we press placeOrderBtn (submit), we need to check if all required fields are filled.
+        //first of all we need to choose them
+        const requiredFields = document.querySelectorAll('input[required]');
+        //then make a counter, which will count required fields that filled.
+        let filledFields = 0;
+
+        requiredFields.forEach(field => {
+            if (field.value.length !== 0) filledFields++;
+        })
+
+        //if all required fields are filled - we placing an oreder,
+        //else user'll see an alert, that says 'All information must be filled'
+        filledFields === requiredFields.length ? placeOrder() : alert('All information must be filled');
+
+        function placeOrder() {
+            //we show user that order has been placed
+            alert('Order has been placed');
+
+            //creating order Object with all info about order.
+            const order = {
+                product: productPage.querySelector('.product-page h1').innerText,
+                name: document.getElementById('name').value,
+                surname: document.getElementById('surname').value,
+                city: document.getElementById('city').value,
+                post_office: document.getElementById('post-office').value,
+                payment: document.querySelector('input[name=payment]:checked').id,
+                comment: document.getElementById('comment').value,
+                ammount: document.getElementById('ammount').value
+            };
+
+            //if user choosed card payment -- adding card information to order Object.
+            if (order.payment === 'card') {
+                order.card_number = document.getElementById('card-number').value;
+                order.expiration_date = document.getElementById('expiration').value;
+                order.cvv = document.getElementById('cvv').value;
+            }
+
+            //removing all the elements from html
+            nav.remove();
+            productsDiv.remove();
+            productPage.remove();
+
+            //and rendering table with order info instead.
+            renderTableFromData(order);
+        }
+    });
+}
+
+function renderTableFromData(data) {
+    const table = document.createElement('table');
+    const tbody = document.createElement('tbody');
+
+    for (const key in data) {
+        const tr = document.createElement('tr');
+        const th = document.createElement('th');
+        const td = document.createElement('td');
+
+        th.innerText = key;
+        td.innerText = data[key];
+
+        tr.append(th);
+        tr.append(td);
+
+        tbody.append(tr);
+    }
+
+    table.append(tbody);
+    body.append(table);
+}
+
+addNavigation(shop.categories);
